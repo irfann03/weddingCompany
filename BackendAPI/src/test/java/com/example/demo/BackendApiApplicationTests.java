428044960858fca37cd0aa330c85c66e2ff5a97c
irfann03 <ahamedirfan375@gmail.com>
2025-12-12 02:44:02 +0530

    setup

--- BackendAPI/src/test/java/com/example/demo/BackendApiApplicationTests.java
+++ BackendAPI/src/test/java/com/example/demo/BackendApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Backend;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendApiApplicationTests {
 
 	@Test
 	void contextLoads() {
